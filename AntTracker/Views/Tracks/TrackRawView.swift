d197ba02e9c55fb34e34c06816f0f3321ca022a1
Vladimir Tezin <vtezin@gmail.com>
2021-05-27 10:01:31 +0300

    file added

---
+++ AntTracker/Views/Tracks/TrackRawView.swift
@@ -0,0 +1,42 @@
+//
+//  TrackRawView.swift
+//  AntTracker
+//
+//  Created by test on 26.05.2021.
+//
+
+import SwiftUI
+
+struct TrackRawView: View {
+    
+    let track: Track
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            
+            Text(track.title)
+            HStack{
+                VStack(alignment: .leading){
+                    Text(track.info)
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                }
+                Spacer()
+                VStack(alignment: .trailing){
+                    Text(track.startDate.dateString())
+                    Text(localeDistanceString(distanceMeters: Double(track.totalDistance)))
+                }
+                .modifier(SecondaryInfo())
+            }
+            
+        }
+        
+    }
+}
+
+//struct TrackRawView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TrackRawView()
+//    }
+//}
